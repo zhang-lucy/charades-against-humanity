09efd5578db13da589b842c1626a36b1192e31a3
zhang-lucy <zhangly@mit.edu>
2020-08-22 19:33:33 -0700

    fix add player method

--- app/data/new_player.js
+++ app/data/new_player.js
@@ -2,7 +2,7 @@
 
 function getGame(gameId) {
     if (global.games) {
-        for(var game of global.games) {
+        for (var game of global.games) {
             if (game.id == gameId) {
                 return game;
             }
@@ -24,34 +24,14 @@ module.exports = {
      * operationId: game_new_player
      */
     put: {
-        200: function (req, res, callback) {
-
-            var gameId = req.query.id;
+        200: function (req, res, game, callback) {
             var playerName = req.query.name;
-            var valid = true;
-            var message, status;
 
             // TODO: Change so it's not just team 1
-            var game = getGame(gameId);
-            if (game != null) {
-                var playerId = game.team1.players.length;
-                var players = game.team1.players
-                players.push({"id": playerId, "name":playerName, "teamId":game.team1.id});
-                game.team1.players = players;
-                message = "Player added. Ready to play!";
-            } else {
-                valid = false;
-                message = "Error: Enter a valid game ID";
-            }
-
-            if (valid) {
-                status = 200;
-            } else {
-                status = 400;
-            }
-
-            res.status(status).send(message);
-
+            var playerId = game.team1.players.length;
+            var players = game.team1.players
+            players.push({ "id": playerId, "name": playerName, "teamId": game.team1.id });
+            game.team1.players = players;
         }
     }
 };
